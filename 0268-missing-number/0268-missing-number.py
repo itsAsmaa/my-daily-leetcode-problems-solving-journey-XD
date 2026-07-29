f0013901346e47a07c8969bfaed603356c2ebdc8
Asma'a Fares <faresasmaa205@gmail.com>
2026-07-29 14:57:46 +0300

    Time: 15 ms (30.54%), Space: 13.3 MB (35.84%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+
+        for i,v in enumerate(nums):
+
+            if (i != v) :
+                return v-1
+
+            if v == len(nums)-1:
+                return v+1
+
+
+        
